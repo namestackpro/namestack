21cdd4111889b93eddaabb8bf98386695f093690
cisco <ikezogwokosiso16@gmail.com>
2025-09-10 22:32:42 +0100

    domain upload page added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
+'use client'
+
+
+import FileUpload from "@/conponents/file-upload/file-upload";
 
 export default function Home() {
   return (
-    <div>
-      hello
+    <div className="min-h-full w-[100%] bg-background ">
+      <FileUpload/>
     </div>
   );
 }
