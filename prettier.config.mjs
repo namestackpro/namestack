583d7f8209b217acaf635e83a3397655141359f5
Ope Adeyomoye <victoradeyomoye@yahoo.co.uk>
2025-09-21 09:45:16 +0100

    configure prettier to run pre-commit

---
+++ prettier.config.mjs
@@ -0,0 +1,16 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+export default {
+  experimentalTernaries: true,
+  experimentalOperatorPosition: 'start',
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'none',
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  arrowParens: 'always',
+  endOfLine: 'lf'
+}
